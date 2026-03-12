62823dbfa6ed1c8cee6e54e783baa26a21d08cd2
Bharatula Krishna Aravind <aravindbk21@gmail.com>
2026-03-12 08:11:24 +0530

    message summarization

--- src/AI_Nodes/nodes.py
+++ src/AI_Nodes/nodes.py
@@ -4,12 +4,14 @@ from AI_Tools.tools import MyTools
 from AI_Sys_Prompt.system_prompt_agent import AGENT_DESCRIPTION
 from AI_LLM.agent_llm import MyLLM
 from langgraph.graph import END
+from langmem.short_term import summarize_messages
 
 
 tools=MyTools().getToolsSync()
 llm_factory = MyLLM(temperature=0.7,tools=tools)
 llm = llm_factory.llm_without_tools()
 llm_tools = llm_factory.llm_with_tools()
+llm_summary = llm_factory.llm_for_summary()
 
 def is_tool_required(state: State):
     messages = state["messages"]
@@ -33,12 +35,24 @@ def llm_with_tools(state: State):
         Dict containing updated messages
     """
     # Create the prompt template with system prompt and messages
+    
+    summarization_result = summarize_messages(
+        state["messages"],
+        running_summary=state.get("summary"),
+        model=llm_summary,
+        max_tokens=2000,
+        max_tokens_before_summary=10000,
+        max_summary_tokens=1000
+    )
+    messags_after_summarization = summarization_result.messages
     prompt_template = ChatPromptTemplate.from_messages  ([
-            ("system", AGENT_DESCRIPTION),
-            MessagesPlaceholder(variable_name="messages")
-        ])
+            ("system", AGENT_DESCRIPTION)
+        ]+messags_after_summarization)
     chain = prompt_template.invoke(state)
     response = llm_tools.invoke(chain)
+    state_update = {"messages": [response]}
+    if messags_after_summarization.running_summary:
+        state_update["summary"] = summarization_result.running_summary
     
     # Return the response as a message
-    return {"messages": [response]}
+    return state_update
